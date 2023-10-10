4f9b99a15ed8f2cd9acfbb82a0bc89d9901da01b
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-10-10 17:07:49 -0300

    Product entity created

--- GeekShopping.ProductAPI/Data/SqlServerContext.cs
+++ GeekShopping.ProductAPI/Data/SqlServerContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GeekShopping.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeekShopping.ProductAPI.Data;
 
 public class SqlServerContext : DbContext
 {
-    public SqlServerContext() {}
-
     public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
 }
---
+++ GeekShopping.ProductAPI/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using GeekShopping.ProductAPI.Models.Base.v1;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Models;
+
+[Table("Product")]
+public class Product : BaseEntity
+{
+    [Column("name")]
+    [Required]
+    [StringLength(100, MinimumLength = 5)]
+    public string? Name { get; set; }
+
+    [Column("price")]
+    [Required]
+    [Range(1, 10000)]
+    public decimal Price { get; set; }
+
+    [Column("description")]
+    [StringLength(255)]
+    public string? Description { get; set; }
+
+    [Column("category_name")]
+    [StringLength(50)]
+    public string? CategoryName { get; set;}
+
+    [Column("image_url")]
+    [StringLength(255)]
+    public string? ImageUrl { get; set; }
+}
