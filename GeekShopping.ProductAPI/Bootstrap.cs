7d80a9063fb81df6948022b5cbf76aea928b63ec
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-10-10 16:52:30 -0300

    Created the BaseEntity class and configured access to the SQL Server database

---
+++ GeekShopping.ProductAPI/Bootstrap.cs
@@ -0,0 +1,20 @@
+﻿using GeekShopping.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI;
+
+public static class Bootstrap
+{
+    public static IServiceCollection AddInjections(this IServiceCollection services)
+    {
+        services.AddRepositories();
+
+        return services;
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddDbContext<SqlServerContext>(options =>
+            options.UseSqlServer(Environment.GetEnvironmentVariable("SQLServerConnection:SQLServerConnectionString")));
+    }
+}
---
+++ GeekShopping.ProductAPI/Models/Base/v1/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Models.Base.v1;
+
+public class BaseEntity
+{
+    [Key]
+    [Column("id")]
+    public long Id { get; set; }
+}
