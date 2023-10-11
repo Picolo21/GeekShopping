f0887393c2257d5193777858d8ebb82b4e338232
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-10-11 16:36:36 -0300

    Defining the properties of the IProductRepository interface

--- GeekShopping.ProductAPI/Repositories/Contracts/v1/IProductRepository.cs
+++ GeekShopping.ProductAPI/Repositories/Contracts/v1/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace GeekShopping.ProductAPI.Repositories.Contracts.v1;
+﻿using GeekShopping.ProductAPI.Data.ValueObjects.v1;
+
+namespace GeekShopping.ProductAPI.Repositories.Contracts.v1;
 
 public interface IProductRepository
 {
-
+    Task<IEnumerable<ProductVO>> FindAllAsync();
+    Task<ProductVO> FindByIdAsync(int id);
+    Task<ProductVO> CreateAsync(ProductVO product);
+    Task<ProductVO> UpdateAsync(ProductVO product);
+    Task<bool> DeleteAsync(int id);
 }
